c6c12421db302e0c46e85c4cb2fc1083d618e26e
Gagik-arch <gagik.chilingaryan2020@gmail.com>
2025-11-25 21:21:20 +0400

    asdasd

--- src/components/MenuBar/MenuBar.ts
+++ src/components/MenuBar/MenuBar.ts
@@ -32,7 +32,7 @@ function MenuBar() {
                                         })
                                             .onMount((e) => {
                                                 desktopStore.subscribe((state) => {
-                                                    const app = state.activeApps.find(a => a.id === state.focusedAppId);
+                                                    const app = state.activeApps.find(a => a.window.dom.id === state.focusedAppId);
                                                  
                                                     e.setProps({
                                                         children: [ app?.name || 'Finder' ],
